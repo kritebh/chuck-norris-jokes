6f7df6f9d6695feefcf50d2574a3605fc4589b25
Kritebh <kritebhlaganbibhakar@gmail.com>
2021-06-17 13:47:51 +0530

    twitter button added

--- app.js
+++ app.js
@@ -2,18 +2,26 @@
 
 const jokes =document.getElementById("j")
 const btn=document.getElementById("new")
+const tweet = document.getElementById("tweet")
 
 const generatejokes =()=>{
     fetch('https://api.chucknorris.io/jokes/random')
     .then((res)=> res.json())
     .then((data)=>{
         jokes.innerHTML=data.value
+        tweetJokes(data.value)
     })
     
     
     .catch((error)=>{
         console.log(error);
     })
+}
+
+function tweetJokes(j){
+    
+    tweet.href ='https://twitter.com/intent/tweet?hashtags=jokes&related=chucknorris&text='+j+"&via=kritebh";
+    return false
 }
 
 btn.addEventListener('click',generatejokes)
